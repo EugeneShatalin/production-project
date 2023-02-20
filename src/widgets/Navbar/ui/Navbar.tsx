5d9b609c53f7445c3fc812c990dfd08dc4d4b139
Yevgeniy Shatalin <shatalin_ei@mail.ru>
2023-02-21 00:04:25 +0600

    Sidebar. Application layout.

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -11,7 +11,6 @@ interface NavbarProps {
 const Navbar = ({className}: NavbarProps) => {
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
-            <ThemeSwitcher/>
             <div className={cls.links}>
                 <AppLink theme={AppLinkTheme.PRIMARY} to={'/'} className={cls.mainLink}>Главная</AppLink>
                 <AppLink theme={AppLinkTheme.PRIMARY} to={'/about'}>О сайте</AppLink>
